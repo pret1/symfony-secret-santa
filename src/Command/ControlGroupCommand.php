6fc68f5d7b980f6b801d1cdbe757c3baab64a136
Siarhei Konau <skonau@scnsoft.com>
2023-04-10 15:01:15 +0300

    Edited CLI command 'app:control-group'.

--- src/Command/ControlGroupCommand.php
+++ src/Command/ControlGroupCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Entity\Group;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,16 +11,31 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsCommand(
     name: 'app:control-group',
-    description: 'Add a short description for your command',
+    description: 'You can use CRUD action for entity group via CLI',
 )]
 class ControlGroupCommand extends Command
 {
+
+    private EntityManagerInterface $entityManager;
+    private SerializerInterface $serializer;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer
+    ) {
+        $this->entityManager = $entityManager;
+        parent::__construct();
+        $this->serializer = $serializer;
+    }
+
     protected function configure(): void
     {
         $this
+            ->setHelp('This command allows you to create a group...')
             ->addArgument('name', InputArgument::OPTIONAL, 'Name group')
             ->addOption('add', null, InputOption::VALUE_NONE, 'Add new group')
         ;
@@ -26,6 +43,7 @@ class ControlGroupCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $group = new Group();
         $io = new SymfonyStyle($input, $output);
         $name = $input->getArgument('name');
 
@@ -34,10 +52,20 @@ class ControlGroupCommand extends Command
         }
 
         if ($input->getOption('add')) {
+            $group->setName($name);
+            $this->entityManager->persist($group);
+            $this->entityManager->flush();
 
+            $io->success('You success add new group via CLI command');
+        } else {
+            $groups = $this->entityManager->getRepository(Group::class)->findAll();
+            $serializerGroups = $this->serializer->serialize($groups, 'json', [
+                'groups' => ['group_read'],
+            ]);
+            var_dump(json_decode($serializerGroups));
+
+            $io->success('Now you can see all available groups');
         }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
     }
